94a4f3adfac197c337340951b30694f756cdde19
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-14 06:54:05 +0100

    async/await for POST added

--- POST/public/main.js
+++ POST/public/main.js
@@ -31,6 +31,31 @@ const shortenUrl = () => {
 })
 }
 
+/*  async/await version
+// Asynchronous functions
+const shortenUrl = async () => {
+	const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(url,
+    {
+      method: 'POST',
+      body: data,
+      headers: {
+  'Content-type': 'application/json',
+  'apikey': apiKey
+}
+    });
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+*/ 
+
 // Clear page and call Asynchronous functions
 const displayShortUrl = (event) => {
   event.preventDefault();
